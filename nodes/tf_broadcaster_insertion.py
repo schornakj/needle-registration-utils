080597a62a198c1ae2f98a17be22d977170bc024
schornakj <jgschornak@wpi.edu>
2017-12-19 14:43:06 -0700

    Made things a little more modular

--- nodes/tf_broadcaster_insertion.py
+++ nodes/tf_broadcaster_insertion.py
@@ -10,7 +10,7 @@ def normalize(input):
     return np.array(input / np.linalg.norm(input))
 
 def process_optitrack_axes(data_optitrack,
-                           transform_needle_base_to_needle_tip, transform_optitrack_to_camera,
+                           transform_needle_base_to_needle_tip, transform_camera_to_optitrack,
                            col_start_marker_a, col_end_marker_a,
                            col_start_marker_b, col_end_marker_b,
                            col_start_marker_c, col_end_marker_c):
@@ -33,7 +33,7 @@ def process_optitrack_axes(data_optitrack,
              np.array([0, 0, 0, 1]).reshape((1, 4))), axis=0)
 
         transform_optitrack_to_needle_tip = np.dot(transform_optitrack_to_needle_base, transform_needle_base_to_needle_tip)
-        transform_camera_to_needle_tip = np.dot(np.linalg.inv(transform_optitrack_to_camera), transform_optitrack_to_needle_tip)
+        transform_camera_to_needle_tip = np.dot(transform_camera_to_optitrack, transform_optitrack_to_needle_tip)
 
         transforms_optitrack_to_needle_base.append(transform_optitrack_to_needle_base)
         transforms_optitrack_to_needle_tip.append(transform_optitrack_to_needle_tip)
@@ -42,6 +42,15 @@ def process_optitrack_axes(data_optitrack,
     return np.array(transforms_optitrack_to_needle_base),\
            np.array(transforms_optitrack_to_needle_tip),\
            np.array(transforms_camera_to_needle_tip)
+
+def process_camera_axes(data_camera):
+    transforms_camera_to_needle_tip_measured = []
+    for row in range(0,len(data_camera)):
+        transform_camera_to_needle_tip_measured = np.concatenate(
+            (np.concatenate((np.eye(3), data_camera[row,1:].reshape((3, 1))), axis=1),
+             np.array([0, 0, 0, 1]).reshape((1, 4))), axis=0)
+        transforms_camera_to_needle_tip_measured.append(transform_camera_to_needle_tip_measured)
+    return np.array(transforms_camera_to_needle_tip_measured)
 
 if __name__ == '__main__':
     rospy.init_node('tf_broadcaster_insertion')
@@ -61,104 +70,25 @@ if __name__ == '__main__':
     print(transform_needle_base_to_needle_tip)
 
     trajectory_camera = np.genfromtxt('/media/jgschornak/Data/2017-12-11 Needle Tip Tracking/Trial 1 trajectory.csv', delimiter=',')
-    # trajectory_camera = np.load('/media/jgschornak/Data/2017-11-29 Needle Tip Tracking/Trial 2 trajectory.npz')['trajectory']
-    # print(trajectory_camera)
 
     data_registration = np.load('transform_camera_to_world.npz')
     transform_camera_to_optitrack = data_registration['transform_camera_to_world']
-    # transform_optitrack_to_camera = data_registration['transform_world_to_camera']
 
     print("Transform_camera_to_optitrack")
     print(transform_camera_to_optitrack)
     print("transform_needle_base_to_needle_tip")
     print(transform_needle_base_to_needle_tip)
 
-    # # Publish needle tip pose throughout insertion
-    # for row in range(0, data_optitrack_needle_insertion.shape[0]):
-    #     quaternion_needle_body = np.array([data_optitrack_needle_insertion[row, 22], data_optitrack_needle_insertion[row, 23], data_optitrack_needle_insertion[row, 24], data_optitrack_needle_insertion[row, 25]])
-    #
-    #     position_needle_body = np.array([data_optitrack_needle_insertion[row, 26], data_optitrack_needle_insertion[row, 27], data_optitrack_needle_insertion[row, 28]])
-    #     position_needle_tip = position_needle_body + translation_tip
-    #     if not np.isnan(position_needle_body[0]):
-    #         time = rospy.Time.now()
-    #         broadcaster.sendTransform(position_needle_body, quaternion_needle_body, time, "needle_body", "world")
-    #         broadcaster.sendTransform(position_needle_tip, quaternion_needle_body, time, "needle_tip", "world")
-    #         rospy.sleep(0.1)
+    transforms_optitrack_to_needle_base,\
+    transforms_optitrack_to_needle_tip,\
+    transforms_camera_to_needle_tip = process_optitrack_axes(data_optitrack_needle_insertion,
+                                                             transform_needle_base_to_needle_tip,
+                                                             transform_camera_to_optitrack,
+                                                             8, 11,
+                                                             2, 5,
+                                                             5, 8)
 
-    transforms_optitrack_to_needle_tip = []
-    transforms_camera_to_needle_tip = []
-
-    transforms_camera_to_needle_tip_measured = []
-
-    for row in range(0, len(data_optitrack_needle_insertion)):
-        # print(trajectory_camera[row,:3])
-        # transform_optitrack_to_needle_tip_measured = np.dot(transform_optitrack_to_camera, transform_camera_to_needle_tip_measured)
-
-# # Trial 2 2:36:07, Trial 3 2:44:31
-#         position_marker_a = np.array(data_optitrack_needle_insertion[row, 8:11]).reshape((1, 3))
-#         position_marker_b = np.array(data_optitrack_needle_insertion[row, 2:5]).reshape((1, 3))
-#         position_marker_c = np.array(data_optitrack_needle_insertion[row, 5:8]).reshape((1, 3))
-
-# # Trial 4 2.46.11
-#         position_marker_a = np.array(data_optitrack_needle_insertion[row, 8:11]).reshape((1, 3))
-#         position_marker_b = np.array(data_optitrack_needle_insertion[row, 2:5]).reshape((1, 3))
-#         position_marker_c = np.array(data_optitrack_needle_insertion[row, 5:8]).reshape((1, 3))
-
-        # Trial 5 2:46:56
-        position_marker_a = np.array(data_optitrack_needle_insertion[row, 8:11]).reshape((1, 3))
-        position_marker_b = np.array(data_optitrack_needle_insertion[row, 2:5]).reshape((1, 3))
-        position_marker_c = np.array(data_optitrack_needle_insertion[row, 5:8]).reshape((1, 3))
-
-
-        needle_axis_x = normalize(position_marker_a - position_marker_b).reshape((3, 1))
-        needle_axis_y = normalize(position_marker_c - position_marker_b).reshape((3, 1))
-        needle_axis_z = normalize(np.cross(needle_axis_x.ravel(), needle_axis_y.ravel())).reshape((3, 1))
-        needle_axis_y = normalize(np.cross(needle_axis_z.ravel(), needle_axis_x.ravel())).reshape((3, 1))
-
-        rotation = np.concatenate((needle_axis_x, needle_axis_y, needle_axis_z), axis=1)
-        transform_optitrack_to_needle_base = np.concatenate(
-            (np.concatenate((rotation, position_marker_b.reshape((3, 1))), axis=1),
-             np.array([0, 0, 0, 1]).reshape((1, 4))), axis=0)
-
-        transform_optitrack_to_needle_tip = np.dot(transform_optitrack_to_needle_base, transform_needle_base_to_needle_tip)
-        transform_camera_to_needle_tip = np.dot(transform_camera_to_optitrack, transform_optitrack_to_needle_tip)
-
-        transforms_optitrack_to_needle_tip.append(transform_optitrack_to_needle_tip)
-        transforms_camera_to_needle_tip.append(transform_camera_to_needle_tip)
-        # print("\nOptitrack to tip")
-        # print(transform_optitrack_to_needle_tip)
-        # print("camera to tip")
-        # print(transform_camera_to_needle_tip)
-        # broadcaster.sendTransform(transforms_camera_to_needle_tip[row][0:3, 3].reshape((3, 1)),
-        #                           tf.transformations.quaternion_from_matrix(transform_camera_to_needle_tip), rospy.Time.now(),
-        #                           "needle_tip", "world")
-        # rospy.sleep(0.1)
-
-    for row in range(0,len(trajectory_camera)):
-        transform_camera_to_needle_tip_measured = np.concatenate(
-            (np.concatenate((np.eye(3), trajectory_camera[row,1:].reshape((3, 1))), axis=1),
-             np.array([0, 0, 0, 1]).reshape((1, 4))), axis=0)
-        transforms_camera_to_needle_tip_measured.append(transform_camera_to_needle_tip_measured)
-        # print("camera to tip (meas)")
-        # print(transform_camera_to_needle_tip_measured)
-
-    optitrack_meas = np.array(transforms_camera_to_needle_tip)
-
-
-    plt.figure(1)
-    plt.plot(range(0, len(trajectory_camera)), trajectory_camera[:, 1],  range(0, len(trajectory_camera)), trajectory_camera[:, 2], range(0,len(trajectory_camera)), trajectory_camera[:, 3])
-
-
-    # print(len(optitrack_meas))
-    print(optitrack_meas)
-
-    # plt.figure(2)
-    # plt.plot(range(0, len(optitrack_meas)), optitrack_meas[:,0,3], range(0, len(optitrack_meas)), optitrack_meas[:,1,3], range(0, len(optitrack_meas)), optitrack_meas[:,2,3])
-    # plt.show()
-
-
-
-    # print(len(transforms_camera_to_needle_tip), len(transforms_camera_to_needle_tip_measured))
+    transforms_camera_to_needle_tip_measured = process_camera_axes(trajectory_camera)
 
 
     for row in range(0, min(len(transforms_camera_to_needle_tip), len(transforms_camera_to_needle_tip_measured))):
